7530b83e502c9cba5f2c3fc4dfd47ce15d0f94bb
bearski <santosh.sewlal@gmail.com>
2018-01-30 08:00:03 +0200

    refugee struct update

--- contracts/RefugeeRegister.sol
+++ contracts/RefugeeRegister.sol
@@ -3,25 +3,35 @@ pragma solidity ^0.4.17;
 contract RefugeeRegister {
 
     struct Refugee {
-        uint RefugeeId;
+        string RefugeeId;
         string FirstName;
 		string LastName;
 		string MiddleName;
 		string Gender;
-		uint DateOfBirth;
+		string DateOfBirth;
 		string Nationality;
 		string RefugeeStatus;
+        string refugeePublicKey;
     }
 
     Refugee[] public Refugees;
     
-    mapping (uint => address) public RefugeeToOwner;
-    mapping (address => uint) ownerRefugeeCount;
 
-    function _createRefugee(uint _RefugeeId, string _FirstName, string _LastName, string _MiddleName, string _Gender, uint _DateOfBirth, string _Nationality, string _RefugeeStatus) public returns (uint) {
-        uint id = Refugees.push(Refugee( _RefugeeId, _FirstName, _LastName, _MiddleName, _Gender, _DateOfBirth, _Nationality, _RefugeeStatus)) - 1;
-        RefugeeToOwner[id] = msg.sender;
-        ownerRefugeeCount[msg.sender]++;
+    function _createRefugee(
+        string _RefugeeId, 
+        string _FirstName, 
+        string _LastName, 
+        string _MiddleName, 
+        string _Gender, 
+        string _DateOfBirth, 
+        string _Nationality, 
+        string _RefugeeStatus,
+        string _refugeePublicKey
+    ) 
+        public 
+        returns (uint) 
+    {
+        uint id = Refugees.push(Refugee( _RefugeeId, _FirstName, _LastName, _MiddleName, _Gender, _DateOfBirth, _Nationality, _RefugeeStatus, _refugeePublicKey)) - 1;
        
         return _RefugeeId;
     }
--- test/TestRefugeeRegister.sol
+++ test/TestRefugeeRegister.sol
@@ -5,14 +5,12 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/RefugeeRegister.sol";
 
 contract TestRefugeeRegister {
-  RefugeeRegister refugeeregister = RefugeeRegister(DeployedAddresses.RefugeeRegister());
+  	RefugeeRegister refugeeregister = RefugeeRegister(DeployedAddresses.RefugeeRegister());
 
-// Testing the _createRefugee() function
-function testUserCan_createRefugee() public {
-  uint returnedId = refugeeregister._createRefugee(1, "Rodney", "Pedro", "G", "Male", 60374, "South African", "Active");
-
-  uint expected = 1;
-
-  Assert.equal(returnedId, expected, "Refugee Id matches.");
-}
+	// Testing the _createRefugee() function
+	function testUserCan_createRefugee() public {
+  		string returnedId = refugeeregister._createRefugee("super ID 001", "Rodney", "Pedro", "G", "Male", "2018/01/29", "South African", "Active", "Public Key Goes Here");
+  		// string expected = "super ID 001";
+  		Assert.equal(returnedId, "super ID 001", "Refugee Id matches.");
+	}
 }
